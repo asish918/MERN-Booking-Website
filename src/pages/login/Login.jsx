f38fc0355257bbf00b9433c631aa87ac5f993548
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-01-09 17:54:57 +0530

    Basic login functionality

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
         try {
             const res = await axios.post("http://localhost:8000/api/auth/login", credentials)
-            dispatch({type: "LOGIN_SUCCESS", payload: res.data})
+            dispatch({type: "LOGIN_SUCCESS", payload: res.data.details})
             navigate('/');
 
         } catch (error) {
